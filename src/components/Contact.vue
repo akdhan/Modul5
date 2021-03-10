83345eced93417d8e754433ea2518ec97c9e8776
Akhdaan Wedya Harjuna <52806288+akdhan@users.noreply.github.com>
2021-03-10 12:03:10 +0700

    Add files via upload

---
+++ src/components/Contact.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container">
+        <h1>Contact</h1>
+        This is Contact Page
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: "Contact"
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container">
+        <h1>Home Page</h1>
+        This is Home Page
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: "Home"
+}
+</script>
